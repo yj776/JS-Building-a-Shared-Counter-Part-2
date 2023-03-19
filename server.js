afde1e263d404882374e5a638aa07cf25945d3f6
Panos Kefalidis <2922646+thepeekay@users.noreply.github.com>
2023-03-19 11:11:32 -0700

    Update server.js
    Adding missing variable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const spawn = require('child_process').spawn;
 const liveServer = require('live-server')
 const sleep = time => new Promise(resolve => setTimeout(resolve,time))
+const isWin = process.platform === 'win32'
 async function main(){
     spawn( isWin ? 'npx.cmd' : 'npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit'});
     await sleep(1000) // <-- Wait one second, so that the json-server has started by the time the live-server opens.
